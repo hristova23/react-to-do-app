1e6d377c02f8e1ffc89e62936808d74dfe5bd426
Victoriya Hristova <victoriya.hristova04@gmail.com>
2022-12-23 03:56:10 +0200

    Add task btn implementation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import ToDoList from "./ToDoList";
+import { v4 as uuidv4 } from "uuid"
 
 function App() {
   const [tasks, setTasks] = useState([{id:1,name:'task1',isCompleted:false}, {id:2,name:'task2',isCompleted:true}])
+  const taskNameRef = useRef();
+
+  function handleAddTask(){
+    const name = taskNameRef.current.value
+    
+    setTasks(prevTasks => {
+      return [...prevTasks, {id: uuidv4(), name:name, isCompleted: false}]
+    })
+
+    taskNameRef.current.value = null;
+  }
 
   return (
     <>
     Tasks:
     <ToDoList tasks={tasks}/>
+    <input ref={taskNameRef} type="text" />
+    <button onClick={handleAddTask}>Add Task</button>
     </>
   )
 }
