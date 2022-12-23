e66b6c32813316ac1746ae0ab8d5211e5b22b3e5
Victoriya Hristova <victoriya.hristova04@gmail.com>
2022-12-23 03:44:22 +0200

    List arr of tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, {useState} from "react";
+import ToDoList from "./ToDoList";
+
 function App() {
-  return null
+  const [tasks, setTasks] = useState([{id:1,name:'task1',isCompleted:false}, {id:2,name:'task2',isCompleted:true}])
+
+  return (
+    <>
+    Tasks:
+    <ToDoList tasks={tasks}/>
+    </>
+  )
 }
 
 export default App;
---
+++ src/ToDoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Task from './Task'
+
+export default function ToDoList({tasks}) {
+  return (
+    tasks.map(task => {
+        return <Task task={task}/>
+    })
+  )
+}
