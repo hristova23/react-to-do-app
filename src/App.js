2ff802d606b0299e8fa9c0958493f39f5d936580
Victoriya Hristova <victoriya.hristova04@gmail.com>
2022-12-24 11:21:32 +0200

    Add bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {useState, useRef, useEffect} from "react";
 import ToDoList from "./ToDoList";
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaCheckSquare, FaCalendarAlt, FaSortAmountDownAlt } from 'react-icons/fa';
+import { VscClearAll } from 'react-icons/vsc';
+
 
 function App() {
   const [tasks, setTasks] = useState(() => {
@@ -36,12 +40,63 @@ function App() {
 
   return (
     <>
-    Tasks:
-    <ToDoList tasks={tasks} toggleTask={toggleTask}/>
-    <input ref={taskNameRef} type="text" />
-    <button onClick={handleAddTask}>Add Task</button>
-    <button onClick={handleClearTasks}>Clear</button>
-    <div>{tasks.filter(task => !task.isCompleted).length} tasks left.</div>
+      <section className="vh-100">
+          <div className="container py-5 h-100">
+              <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col">
+                  <div className="card" id="list1" style={{borderRadius: ".75rem", backgroundColor: "#eff1f2"}}>
+                  <div className="card-body py-4 px-4 px-md-5">
+
+                      <p className="h1 text-center mt-3 mb-4 pb-3 text-primary">
+                      <FaCheckSquare />
+                      <u>My Todo-s</u>
+                      </p>
+
+                      <div className="pb-2">
+                      <div className="card">
+                          <div className="card-body">
+                          <div className="d-flex flex-row align-items-center">
+                              <input ref={taskNameRef} type="text" className="form-control form-control-lg"
+                              placeholder="Add new..." />
+                              <a href="#!" data-mdb-toggle="tooltip" title="Set due date">
+                                <FaCalendarAlt size={28} /></a>
+                              <div>
+                              <button onClick={handleAddTask} className="btn btn-primary">Add</button>
+                              </div>
+                          </div>
+                          </div>
+                      </div>
+                      </div>
+
+                      <hr className="my-4" />
+
+                      <div className="d-flex justify-content-end align-items-center mb-4 pt-2 pb-3">
+                        <p className="small mb-0 me-2 text-muted">Filter</p>
+                        <select className="select">
+                            <option value="1">All</option>
+                            <option value="2">Completed</option>
+                            <option value="3">Incompleted</option>
+                        </select>
+                        <p className="small mb-0 ms-4 me-2 text-muted">Sort</p>
+                        <select className="select">
+                            <option value="1">Recent</option>
+                            <option value="2">Older</option>
+                        </select>
+                        <a href="#!" style={{color:"#23af89"}} data-mdb-toggle="tooltip" title="Ascending">
+                          <FaSortAmountDownAlt />
+                          <VscClearAll size={20} color={"red"} onClick={handleClearTasks} />
+                        </a>
+                        </div>
+
+                      <ToDoList tasks={tasks} toggleTask={toggleTask}/>
+
+                      <div>{tasks.filter(task => !task.isCompleted).length} tasks left.</div>
+                  </div>
+                  </div>
+              </div>
+              </div>
+          </div>
+      </section>
     </>
   )
 }
--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaTrashAlt } from 'react-icons/fa';
 
 export default function Task({task, toggleTask}) {
 
@@ -7,11 +8,29 @@ export default function Task({task, toggleTask}) {
   }
 
   return (
-    <div>
-      <label>
-        <input type="checkbox" checked={task.isCompleted} onChange={handleTaskClick} />
-        {task.name}
-      </label>
-    </div>
+    <>
+      <ul className="list-group list-group-horizontal rounded-0 bg-transparent">
+        <li className="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent">
+            <div className="form-check">
+            <input className="form-check-input me-0" type="checkbox"
+                aria-label="..." checked={task.isCompleted} onChange={handleTaskClick} />
+            </div>
+        </li>
+        <li className="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent">
+            <p className="lead fw-normal mb-0">{task.name}</p>
+        </li>
+        <li className="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent">
+            <div className="d-flex flex-row justify-content-end mb-1">
+            <a href="#!" className="text-danger" data-mdb-toggle="tooltip" title="Delete todo"><FaTrashAlt />
+            </a>
+            </div>
+            <div className="text-end text-muted">
+            <a href="#!" className="text-muted" data-mdb-toggle="tooltip" title="Created date">
+                <p className="small mb-0"><i className="fas fa-info-circle me-2"></i>24th Dec 2022</p>
+            </a>
+            </div>
+        </li>
+      </ul>
+    </>
   )
 }
